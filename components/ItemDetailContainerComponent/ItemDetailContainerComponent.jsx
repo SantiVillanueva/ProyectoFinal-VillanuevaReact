4018196ef2023c73ac79287557c710b2fe4b3f47
Santiago Villanueva <santvillanueva22@gmail.com>
2023-03-25 11:11:21 -0300

    Initialize project using Create React App

---
+++ components/ItemDetailContainerComponent/ItemDetailContainerComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ItemDetailContainerComponent = () => {
+  return (
+    <div>ItemDetailContainerComponent</div>
+  )
+}
---
+++ json/products.js
@@ -0,0 +1,81 @@
+export const productsData = [
+{
+    "id":1,
+    "categoria": "Remera",
+    "imagen": "../img/products/01.jpg",
+    "modelo":"Messi con la Copa",
+    "precio": 4000,
+    "stock":20
+},
+{
+    "id":3,
+    "categoria": "Remera",
+    "imagen": "../img/products/03.jpg",
+    "modelo":"anda pa alla",
+    "precio":3800,
+    "stock":8
+},
+
+ {
+    "id":2,
+    "categoria": "Remera",
+    "imagen": "../img/products/02.jpg",
+    "modelo":"Messi",
+    "precio":3500,
+    "stock":12
+},
+{
+    "id":4,
+    "categoria": "Remera",
+    "imagen": "../img/products/04.jpg",
+    "modelo":"Cerati",
+    "precio":3700 ,
+    "stock":12
+},
+{
+    "id":5,
+    "categoria": "Gorra",
+    "imagen": "../img/products/05.jpg",
+    "modelo":"Maradona",
+    "precio":4200 ,
+    "stock":15
+},
+{
+    "id":6,
+    "categoria": "Gorra",
+    "imagen": "../img/products/06.jpg",
+    "modelo":"La Renga",
+    "precio":3750 ,
+    "stock":20
+},
+{
+    "id":7,
+    "categoria": "Gorra",
+    "imagen": "../img/products/07.jpg",
+    "modelo":"Cancerbero",
+    "precio":4000 ,
+    "stock":0
+},
+{
+    "id":8,
+    "categoria": "Buzo",
+    "imagen": "../img/products/08.jpg",
+    "modelo":"Andres Calamaro",
+    "precio":3600 ,
+    "stock":8
+},
+{
+    "id":9,
+    "categoria": "Buzo",
+    "imagen": "../img/products/09.jpg",
+    "modelo":"Muzza y Vermu",
+    "precio":3500 ,
+    "stock":30
+},
+
+]
+
+
+
+
+
